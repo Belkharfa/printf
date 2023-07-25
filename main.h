d5d819fb336ce834eb4dd1a9e27a9f82228c6a05
belkharfa <belkharfa.saad@gmail.com>
2023-07-24 18:06:35 -0700

    task 0000

--- main.h
+++ main.h
@@ -30,7 +30,4 @@ int _hex_u(va_list hexa);
 int _strlen(char *s);
 int _bin(va_list bin);
 int _putchar(char c);
-int print_string(va_list types, char buffer[],
-        int flags, int width, int precision, int size);
-int get_flags(const char *format, int *i);
 #endif
